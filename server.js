a18083106e4d3a095735ca9756f270465f0b09a5
MalavMori <92566896+MalavMori@users.noreply.github.com>
2025-02-16 17:39:40 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const app = express()
+
+const cors = require("cors")
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server,{
+    cors:{
+        origin:"*",
+        methods:["GET","POST"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log("user: "+socket.id)
+    socket.on("message",(data)=>{
+        console.log(data)
+    })
+})
+
+server.listen(3001,()=>{
+    console.log("Server on 3001")
+})
